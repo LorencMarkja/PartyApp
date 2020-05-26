1ce0c461f8f69bcf5381c9bcb8265e52963f131f
davide988 <58575400+davide988@users.noreply.github.com>
2020-05-26 19:40:41 +0200

    commit api create

--- htdocs/auth_app/api/create.php
+++ htdocs/auth_app/api/create.php
@@ -1,32 +1,32 @@
 <?php
-#<!-- This is protected route. Accessed by only loggged in users -->
 include_once 'config/dbh.php';
-include_once '../vendor/autoload.php';
-
-use \Firebase\JWT\JWT;
-
 include_once 'config/cors.php';
 
-// get request headers
-$authHeader = getallheaders();
-if (isset($authHeader['Authorization']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
-    $token = $authHeader['Authorization'];
-    $token = explode(" ", $token)[1];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = json_decode(file_get_contents("php://input"));
 
-    try {
-        $key = "YOUR_SECRET_KEY";
-        $decoded = JWT::decode($token, $key, array('HS256'));
+    $name = $data->name;
+	$description = $data->description;
+	$genre = $data->genre;
+    $mood = $data->mood;
+    $startT = $data->startT;
+    $endT = $data->endT;
 
-        // Do some actions if token decoded successfully.
 
-        // But for this demo let return decoded data
-        http_response_code(200);
-        echo json_encode($decoded);
-    } catch (Exception $e) {
-        http_response_code(401);
-        echo json_encode(array('message' => 'Please authenticate'));
+
+    // U can do validation like unique username etc....
+
+
+    $sql = $conn->query("INSERT INTO party (name, description, genre, mood, startT, endT) VALUES ('$name', '$description', '$genre', '$mood', '$startT', '$endT')");
+    if ($sql) {
+        http_response_code(201);
+        echo json_encode(array('message' => 'Party created'));
+    } else {
+        http_response_code(500);
+       // echo json_encode(array('message' => 'Internal Server error  '));
+       echo "Il mio nome è $name";
+
     }
 } else {
-    http_response_code(401);
-    echo json_encode(array('message' => 'Please authenticate'));
+    http_response_code(405);
 }
