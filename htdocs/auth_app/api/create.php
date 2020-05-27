ebaafe08f5c954b3b97b7921781f1feef50ca5f4
davide988 <58575400+davide988@users.noreply.github.com>
2020-05-27 12:21:44 +0200

    commit node

--- htdocs/auth_app/api/create.php
+++ htdocs/auth_app/api/create.php
@@ -1,32 +1,32 @@
 <?php
+#<!-- This is protected route. Accessed by only loggged in users -->
 include_once 'config/dbh.php';
+include_once '../vendor/autoload.php';
+
+use \Firebase\JWT\JWT;
+
 include_once 'config/cors.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $data = json_decode(file_get_contents("php://input"));
+// get request headers
+$authHeader = getallheaders();
+if (isset($authHeader['Authorization']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+    $token = $authHeader['Authorization'];
+    $token = explode(" ", $token)[1];
 
-    $name = $data->name;
-	$description = $data->description;
-	$genre = $data->genre;
-    $mood = $data->mood;
-    $startT = $data->startT;
-    $endT = $data->endT;
+    try {
+        $key = "YOUR_SECRET_KEY";
+        $decoded = JWT::decode($token, $key, array('HS256'));
 
+        // Do some actions if token decoded successfully.
 
-
-    // U can do validation like unique username etc....
-
-
-    $sql = $conn->query("INSERT INTO party (name, description, genre, mood, startT, endT) VALUES ('$name', '$description', '$genre', '$mood', '$startT', '$endT')");
-    if ($sql) {
-        http_response_code(201);
-        echo json_encode(array('message' => 'Party created'));
-    } else {
-        http_response_code(500);
-       // echo json_encode(array('message' => 'Internal Server error  '));
-       echo "Il mio nome è $name";
-
+        // But for this demo let return decoded data
+        http_response_code(200);
+        echo json_encode($decoded);
+    } catch (Exception $e) {
+        http_response_code(401);
+        echo json_encode(array('message' => 'Please authenticate'));
     }
 } else {
-    http_response_code(405);
+    http_response_code(401);
+    echo json_encode(array('message' => 'Please authenticate'));
 }
---
+++ htdocs/auth_app/api/createP.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'config/dbh.php';
+include_once 'config/cors.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = json_decode(file_get_contents("php://input"));
+
+    $name = $data->name;
+	$description = $data->description;
+	$genre = $data->genre;
+    $mood = $data->mood;
+
+
+
+
+
+    // U can do validation like unique username etc....
+
+
+    $sql = $conn->query("INSERT INTO party (name, description, genre, mood) VALUES ('$name', '$description', '$genre', '$mood')");
+    if ($sql) {
+        http_response_code(201);
+        echo json_encode(array('message' => 'Party created'));
+    } else {
+        http_response_code(500);
+        echo json_encode(array('message' => 'Internal Server error  '));
+      // echo " $name, $description, $genre, $mood, $startT, $endT ";
+
+    }
+} else {
+    http_response_code(405);
+}
