c0f80c1b2aeb10b04fdc1b3e13b8b12b58067dd8
Lorenc <lorenc.markja@student.univaq.it>
2020-06-02 14:45:50 +0200

    commit profilo

--- src/app/user-area/user-area.page.ts
+++ src/app/user-area/user-area.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Key } from 'protractor';
+import { Router } from '@angular/router';
+import * as jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-user-area',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAreaPage implements OnInit {
 
-  constructor() { }
+  Uid: any ;
+  name: any;
+  lastname: any;
+  username: any;
 
-  ngOnInit() {
-  }
+  constructor(
+      private http: HttpClient,
+      private router: Router
+  ) { }
 
-}
+  ngOnInit() {}
+
+  ionViewDidEnter(){
+    var token = localStorage.getItem('token');
+    var decoded = jwt_decode(token);
+    console.log(decoded);
+
+    this.name=decoded['firstname'];
+    this.lastname=decoded['lastname'];
+    this.username=decoded['username'];
+    this.Uid=decoded['user_id'];
+
+}}
