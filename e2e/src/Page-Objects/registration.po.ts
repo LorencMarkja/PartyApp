194fcef389e0c9dcd24de9a1677dcc2913334807
Emagonzales <61739253+Emagonzales@users.noreply.github.com>
2020-06-02 14:42:51 +0200

    e2e testing

---
+++ e2e/src/Page-Objects/registration.po.ts
@@ -0,0 +1,39 @@
+import { browser, by, element, ExpectedConditions } from 'protractor';
+
+export class RegistrationPO {
+
+    browseToPage(){
+        browser.manage().window().setSize(400, 720);
+        browser.get('/register');
+        browser.driver.sleep(500);
+    }
+
+    getTitle(){
+        return element(by.deepCss('ion-title')).getText();
+    }
+
+    async fillForm(){
+        await element(by.css('ion-input[formControlName="firstname"] input')).sendKeys('Prova');
+        await element(by.css('ion-input[formControlName="lastname"] input')).sendKeys('Prova');
+        await element(by.css('ion-input[formControlName="username"] input')).sendKeys('Padmin');
+        await element(by.css('ion-input[formControlName="password"] input')).sendKeys('12345');
+        browser.sleep(200);
+        await element(by.css('[type="submit"]')).click();
+        browser.sleep(200);
+    }
+
+    async fillFormInvalidData(){
+        await element(by.css('ion-input[formControlName="firstname"] input')).sendKeys('Prova');
+        await element(by.css('ion-input[formControlName="lastname"] input')).sendKeys('Prova');
+        await element(by.css('ion-input[formControlName="username"] input')).sendKeys('admin');
+        await element(by.css('ion-input[formControlName="password"] input')).sendKeys('12345');
+        browser.sleep(200);
+        await element(by.css('[type="submit"]')).click();
+        browser.sleep(200);
+    }
+
+    getAlert(){
+        return element(by.css('.alert-wrapper')).getText();
+    }
+
+}
